e9e8e576c8bcecb3082becee545e84aa08dc1fa7
Justinas Marcinka <jumar@dbml.dk>
2018-03-07 10:06:10 +0200

    Configure persistence for Author entity

--- src/main/java/lt/vu/entities/Author.java
+++ src/main/java/lt/vu/entities/Author.java
@@ -1,16 +1,40 @@
 package lt.vu.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+@Table(name = "AUTHOR")
 public class Author implements Serializable {
 
     public Author() {}
+
     public Author(String name) {
-       this.name = name;
+        this.name = name;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "NAME")
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 
     public String getName() {
         return name;
@@ -18,5 +42,13 @@ public class Author implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
